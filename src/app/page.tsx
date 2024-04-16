e1737dab85027e1dfe72b717566ac4c0a8489669
Yuriy Pilipenko <yurik2061@gmail.com>
2024-04-16 11:06:19 +0300

    i modern-normalize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <main>
       blackbook
+      <UserButton 
+            afterSignOutUrl="/sign-in"/>
+
     </main>
   );
 }
