f570f2b7257d8c5d17cf8d17edb7829f17faea8b
Yuriy Pilipenko <yurik2061@gmail.com>
2024-05-02 09:41:44 +0300

    ready

--- src/app/happy/page.tsx
+++ src/app/happy/page.tsx
@@ -1,4 +1,3 @@
-
 import CoatOfArms from '@/components/icons/coatOfArms'
 import Image from 'next/image'
 import React from 'react'
@@ -7,18 +6,18 @@ function page() {
   return (
     <div className=''>
       <div className='h-wr'>
-          <div className='happy relative bg-gradient-to-r from-indigo-400 to-green-800'>
+				<div className='happy relative bg-gradient-to-r from-indigo-400 to-green-800'>
             З Днем народження ! 
             <Image  src='https://res.cloudinary.com/dwdkw1a4j/image/upload/v1714576993/happy%20birthday/eretpddh6rv8r0veb26c.png'
              alt='alt' 
              width={60}  
              height={120}/>
-
-            {/* <div className='circle-r  bg-green-700 '><CoatOfArms  /></div> */}
-            </div>
-
-              </div>
-      </div>
+            <div className='circle-r  bg-green-700 '>
+							<CoatOfArms  />
+					</div>
+				</div>
+			</div>
+		</div>
   )
 }
 
